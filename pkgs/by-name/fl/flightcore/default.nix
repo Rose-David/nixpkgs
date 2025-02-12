465e31205deeabfba5d5ea87c0d4633e6ed1cdc7
Diana Rose <teknolog1kal@protonmail.com>
2025-02-12 12:37:12 -0500

    Copied general structure from pkgs.gale

--- pkgs/by-name/fl/flightcore/default.nix
+++ pkgs/by-name/fl/flightcore/default.nix
@@ -3,6 +3,11 @@
   stdenv,
   rustPlatform,
   fetchFromGitHub,
+  fetchNpmDeps,
+  npmHooks,
+  cargo-tauri,
+  nodejs,
+
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -16,6 +21,26 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   };
 
+  npmDeps = fetchNpmDeps {
+    name = "${finalAttrs.pname}- ${finalAttrs.version}-npm-deps";
+    inherit (finalAttrs) src;
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      cargoRoot
+      ;
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  cargoRoot = "src-tauri";
+
+  buildAndTestSubdir = finalAttrs.cargoRoot;
+
   nativeBuildInputs = [
 
   ];
@@ -26,6 +51,7 @@ stdenv.mkDerivation (finalAttrs: {
   meta = {
     description = "An installer, updater, and mod-manager for Titanfall 2's Northstar Mod.";
     homepage = "https://github.com/R2NorthstarTools/FlightCore";
+    mainProgram = "flightcore";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ teknolog1k ];
   };
